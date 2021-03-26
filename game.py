496000c35dcd08e1e59cf0e6e84c1c66215b56aa
Sabcomp <alushisabrina93@gmail.com>
2021-03-26 21:36:33 +0000

    Create the final version of game.py

--- game.py
+++ game.py
@@ -1,1 +1,6 @@
+verb_ed = input("Enter a verb in -ed: ")
+verb = input("Enter a verb: ")
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
 
+print("One thing I've always " + verb_ed + " about is whether unicorns " + verb + " rainbows, or is their " + noun + " " + adjective + " like any other animal's?")
